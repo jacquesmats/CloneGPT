28686d9127c122bf9756077f29de846c3b6f60b7
Matheus Jacques <matheus.jacques@live.com>
2025-03-13 10:41:00 +0000

    fix hydratation error on landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,21 @@
+"use client"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import LoadingIndicator from "@/components/ui/LoadingIndicator";
 import LandingPage from "@/components/LandingPag";
 
 export default function Home() {
+  const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Return loading state until client-side code has executed
+  if (!mounted) {
+    return <LoadingIndicator fullScreen message="Loading..." />;
+  }
+
   return <LandingPage />;
 }
